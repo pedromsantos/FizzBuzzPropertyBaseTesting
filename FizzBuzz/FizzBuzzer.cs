97b339eef027865b22098b636432ead540b08d9a
Pedro Santos <pedro@bytesteam.com>
2015-04-27 17:32:26 +0100

    Created extension method divisible by on int.

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -4,24 +4,35 @@ namespace FizzBuzz
 {
     public class FizzBuzzer
     {
+        private const int FizzDivisor = 3;
+        private const int BuzzDivisor = 5;
+
         public string Translate(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0)
+            if (number.DivisibleBy(FizzDivisor) && number.DivisibleBy(BuzzDivisor))
             {
                 return "FizzBuzz";
             }
 
-            if (number % 5 == 0)
+            if (number.DivisibleBy(BuzzDivisor))
             {
                 return "Buzz";
             }
 
-            if (number % 3 == 0)
+            if (number.DivisibleBy(FizzDivisor))
             {
                 return "Fizz";
             }
 
             return number.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    public static class Divisible
+    {
+        public static bool DivisibleBy(this int number, int divisor)
+        {
+            return number % divisor == 0;
         }
     }
 }
